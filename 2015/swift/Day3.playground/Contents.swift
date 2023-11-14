886864677fe6860c3e9d9fc99baa2495ffb1799b
Martin Linklater <mslinklater@gmail.com>
2023-11-14 13:13:01 +0000

    2015 Day 3 done

--- 2015/swift/Day3.playground/Contents.swift
+++ 2015/swift/Day3.playground/Contents.swift
@@ -4,35 +4,49 @@ import Cocoa
 let inputUrl = Bundle.main.url(forResource: "input", withExtension: "")
 let input = try String(contentsOf: inputUrl!)
 
-// part 1
-var presentCount: [String:Int] = [:]
-var xLoc: Int = 0
-var yLoc: Int = 0
-for direction in input {
-    // deliver present to current location
-    let locKey = "\(xLoc)-\(yLoc)"
-    if presentCount[locKey] == nil {
-        presentCount[locKey] = 1
-    } else {
-        var currentOpt = presentCount[locKey]
-        if let current = currentOpt {
-            presentCount[locKey] = current + 1
+func solve(numSantas: Int) -> Int
+{
+    var presentCount: [String:Int] = [:]
+    var xLoc: [Int] = []
+    var yLoc: [Int] = []
+    
+    // initialise
+    for _ in 0...numSantas {
+        xLoc.append(0)
+        yLoc.append(1)
+    }
+    var iSanta = 0
+    for direction in input {
+        // deliver present to current location
+        let locKey = "\(xLoc[iSanta])-\(yLoc[iSanta])"
+        if presentCount[locKey] == nil {
+            presentCount[locKey] = 1
         } else {
-            assert(false, "presentCount contained non-Int value")
+            var currentOpt = presentCount[locKey]
+            if let current = currentOpt {
+                presentCount[locKey] = current + 1
+            } else {
+                assert(false, "presentCount contained non-Int value")
+            }
         }
+        switch direction {
+        case "^":
+            yLoc[iSanta] -= 1
+        case "v":
+            yLoc[iSanta] += 1
+        case "<":
+            xLoc[iSanta] -= 1
+        case ">":
+            xLoc[iSanta] += 1
+        default:
+            break
+        }
+        iSanta += 1
+        iSanta %= numSantas
     }
-    switch direction {
-    case "^":
-        yLoc -= 1
-    case "v":
-        yLoc += 1
-    case "<":
-        xLoc -= 1
-    case ">":
-        xLoc += 1
-    default:
-        break
-    }
+
+    return presentCount.count
 }
 
-print("Answer 1:\(presentCount.count)")
+print("Answer 1:\(solve(numSantas:1))")
+print("Answer 2:\(solve(numSantas:2))")
