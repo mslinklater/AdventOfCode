1933b31a0994d4bf85144f05bed25461d4f04791
Martin Linklater <mslinklater@gmail.com>
2024-12-04 12:54:48 +0000

    Day 5 boilerplate

---
+++ 2024/day5.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <string>
+#include <algorithm>
+
+#include "utils.h"
+
+int Solve(const std::string& filename, int part)
+{
+    int answer = 0;
+
+
+    return answer;
+}
+
+int main(int argc, const char** argv)
+{
+//    assert(Solve("../input/day5test", 1) == 0);
+//    assert(Solve("../input/day5test", 2) == 0);
+    
+//    assert(Solve("../input/day5", 1) == 0);
+//    assert(Solve("../input/day5", 2) == 0);
+
+//    std::cout << "problem 1:" << Solve("../input/day5", 1) << std::endl;
+//    std::cout << "problem 2:" << Solve("../input/day5", 2) << std::endl;
+
+    return 0;
+}
