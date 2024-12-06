5b7f9e750eeb503d9dc7114af98d3b61b73403b9
Martin Linklater <mslinklater@gmail.com>
2024-12-06 20:41:17 +0000

    plap

--- 2024/day6.cpp
+++ 2024/day6.cpp
@@ -37,9 +37,101 @@ public:
         return e[y][x];
     }
 
+    int Height()
+    {
+        return e.size();
+    }
+
+    int Width()
+    {
+        return e[0].size();
+    }
+
 private:
     std::vector<std::vector<T>> e;
 };
+
+//-------------------------
+
+bool Walk(Grid2D<Cell>& grid)
+{
+    Vec2 pos;
+    EDirection dir = EDirection::Up;
+
+    for(int y = 0; y < grid.Height() ; y++)
+    {
+        for(int x = 0; x < grid.Width() ; x++)
+        {
+            Cell& c = grid.GetMutable(x,y);
+            c.visited = false;
+            if(c.c == '^')
+            {
+                pos.x = x;
+                pos.y = y;
+                c.visited = true;
+            }
+        }
+    }
+    // clear occupancy
+    // find pos
+    // set direction
+
+    int width = grid.Width();
+    int height = grid.Height();
+
+    while(pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height)
+    {
+        Cell& cellRef = grid.GetMutable(pos.x, pos.y);
+        cellRef.visited = true;
+        
+        switch(dir)
+        {
+            case EDirection::Up:
+                if((pos.y == 0) || (grid.Get(pos.x, pos.y - 1).c != '#'))
+                {
+                    pos.y -= 1;
+                }
+                else
+                {
+                    dir = EDirection::Right;
+                }
+                break;
+            case EDirection::Right:
+                if((pos.x == width-1) || (grid.Get(pos.x + 1, pos.y).c != '#'))
+                {
+                    pos.x += 1;
+                }
+                else
+                {
+                    dir = EDirection::Down;
+                }
+                break;
+            case EDirection::Down:
+                if((pos.y == height-1) || (grid.Get(pos.x, pos.y + 1).c != '#'))
+                {
+                    pos.y += 1;
+                }
+                else
+                {
+                    dir = EDirection::Left;
+                }
+                break;
+            case EDirection::Left:
+                if((pos.x == 0) || (grid.Get(pos.x - 1, pos.y).c != '#'))
+                {
+                    pos.x -= 1;
+                }
+                else
+                {
+                    dir = EDirection::Up;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return false;
+}
 
 //-------------------------
 
@@ -47,7 +139,6 @@ int Solve(const std::string& filename, int part)
 {
     int answer = 0;
     Vec2 pos;
-    EDirection dir = EDirection::Up;
     StringVector map = GetFileAsLines(filename);
     int width = map[0].size();
     int height = map.size();
@@ -60,67 +151,25 @@ int Solve(const std::string& filename, int part)
             Cell c;
             c.c = map[y][x];
             c.visited = false;
-            if(c.c == '^')
-            {
-                pos.x = x;
-                pos.y = y;
-                c.visited = true;
-            }
+//            if(c.c == '^')
+//            {
+//                pos.x = x;
+//                pos.y = y;
+//                c.visited = true;
+//            }
             grid.Set(x, y, c);
         }
     }
 
     if(part == 1)
     {
-        while(pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height)
+        Walk(grid);
+        for(int x=0 ; x<width ; ++x)
         {
-            Cell& cellRef = grid.GetMutable(pos.x, pos.y);
-            cellRef.visited = true;
-            
-            switch(dir)
+            for(int y=0 ; y<height ; y++)
             {
-                case EDirection::Up:
-                    if(grid.Get(pos.x, pos.y - 1).c != '#')
-                    {
-                        pos.y -= 1;
-                    }
-                    else
-                    {
-                        dir = EDirection::Right;
-                    }
-                    break;
-                case EDirection::Right:
-                    if(grid.Get(pos.x + 1, pos.y).c != '#')
-                    {
-                        pos.x += 1;
-                    }
-                    else
-                    {
-                        dir = EDirection::Down;
-                    }
-                    break;
-                case EDirection::Down:
-                    if(grid.Get(pos.x, pos.y + 1).c != '#')
-                    {
-                        pos.y += 1;
-                    }
-                    else
-                    {
-                        dir = EDirection::Left;
-                    }
-                    break;
-                case EDirection::Left:
-                    if(grid.Get(pos.x - 1, pos.y).c != '#')
-                    {
-                        pos.x -= 1;
-                    }
-                    else
-                    {
-                        dir = EDirection::Up;
-                    }
-                    break;
-                default:
-                    break;
+                if(grid.Get(x,y).visited)
+                    answer++;
             }
         }
     }
@@ -131,9 +180,9 @@ int Solve(const std::string& filename, int part)
 int main(int argc, const char** argv)
 {
     assert(Solve("../input/day6test", 1) == 41);
-//    assert(Solve("../input/day5test", 2) == 123);
+//    assert(Solve("../input/day5test", 2) == 6);
     
-//    assert(Solve("../input/day5", 1) == 5091);
+    assert(Solve("../input/day5", 1) == 4973);
 //    assert(Solve("../input/day5", 2) == 4681);
 
     std::cout << "problem 1:" << Solve("../input/day6", 1) << std::endl;
