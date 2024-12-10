18b66f6e29272c87091266b01770deff97593e43
Martin Linklater <mslinklater@gmail.com>
2024-12-10 14:04:12 +0000

    lunch tinkering

--- 2024/day9.cpp
+++ 2024/day9.cpp
@@ -4,6 +4,12 @@
 
 #include "utils.h"
 
+struct Range
+{
+    int value;
+    int count;
+};
+
 uint64_t Solve(const std::string& filename, int part)
 {
     uint64_t answer = 0;
@@ -37,6 +43,7 @@ uint64_t Solve(const std::string& filename, int part)
 
     if(part == 1)
     {
+        // simple char by char move
         int head = 0;
         int tail = blocks.size() - 1;
         while(head < tail)
@@ -48,53 +55,21 @@ uint64_t Solve(const std::string& filename, int part)
             blocks[head] = blocks[tail];
             blocks[tail] = -1;
         }
+        // clean up back end        
+        while(blocks[blocks.size() - 1] == -1)
+        {
+            blocks.pop_back();
+        }
+        // work out answer
+        for(int i=0 ; i<blocks.size() ; i++)
+        {
+            answer += blocks[i] * i;
+        }
     }
     else
     {
-        int frontHead = 0;
-        int frontTail = 0;
-        int backHead = blocks.size() - 1;
-        int backTail = blocks.size() - 1;
-
-        while(frontHead < backHead)
-        {
-            // find tail range
-            while(blocks[backTail] == -1)
-                backTail--;
-            backHead = backTail;
-            while(blocks[backHead] == blocks[backTail])
-                backHead--;
-            backHead++;
-            int gapRequired = backTail - backHead + 1;
-
-            int gapAvailable = 0;
-
-            do
-            {
-                // find a gap big enough
-                while(blocks[frontTail] != -1)
-                    frontTail++;
-                frontHead = frontTail;
-                while(blocks[frontHead] == -1)
-                    frontHead++;
-                frontHead--;
-                gapAvailable = frontHead - frontTail + 1;
-            } while (gapAvailable < gapRequired);
-
-
-        }
-
     }
 
-    while(blocks[blocks.size() - 1] == -1)
-    {
-        blocks.pop_back();
-    }
-
-    for(int i=0 ; i<blocks.size() ; i++)
-    {
-        answer += blocks[i] * i;
-    }
 
     return answer;
 }
@@ -108,7 +83,66 @@ int main(int argc, const char** argv)
 //    assert(Solve("../input/day8", 2) == 1280);
 
     std::cout << "problem 1:" << Solve("../input/day9", 1) << std::endl;
-    std::cout << "problem 2:" << Solve("../input/day9", 2) << std::endl;
+//    std::cout << "problem 2:" << Solve("../input/day9", 2) << std::endl;
 
     return 0;
 }
+
+//--- TODELETE
+
+#if 0
+        int frontHead = 0;
+        int frontTail = 0;
+        int backHead = blocks.size() - 1;
+        int backTail = blocks.size() - 1;
+
+        while(backHead > 0)
+        {
+            // find tail range
+            while(blocks[backTail] == -1)
+                backTail--;
+            backHead = backTail;
+            while(blocks[backHead] == blocks[backTail])
+                backHead--;
+            backHead++;
+            int gapRequired = backTail - backHead + 1;
+
+            int gapAvailable = 0;
+
+            bool foundSlot = false;
+            do
+            {
+                // find a gap big enough
+                while(blocks[frontTail] != -1)
+                    frontTail++;
+                frontHead = frontTail;
+                while(blocks[frontHead] == -1)
+                    frontHead++;
+                frontHead--;
+                gapAvailable = frontHead - frontTail + 1;
+                if(gapAvailable >= gapRequired)
+                {
+//                    frontTail = frontHead+1;
+                    foundSlot = true;
+                }
+                else
+                {
+                    frontTail = frontHead+1;
+                }
+            } while ((gapAvailable < gapRequired) && !foundSlot && (frontTail < backHead));
+
+            if(foundSlot)
+            {
+                for(int i=0; i<gapRequired ; i++)
+                {
+                    blocks[frontTail+i] = blocks[backTail-i];
+                    blocks[backTail-i] = -1;
+                }
+//                frontHead = frontTail + gapRequired;
+            }
+                backHead--;
+                backTail = backHead;
+//            }
+            frontHead = frontTail = 0;
+        }
+#endif
