c2cba7a84bced88f5373a2b4ad4f6372cb4bb73c
Martin Linklater <mslinklater@gmail.com>
2024-12-11 08:17:09 +0000

    backup

--- 2024/day9.cpp
+++ 2024/day9.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <string>
 #include <algorithm>
+#include <list>
 
 #include "utils.h"
 
@@ -10,24 +11,48 @@ struct Range
     int count;
 };
 
+void PrintRange(const std::list<Range>& ranges)
+{
+    int size = 0;
+    int occupiedSize = 0;
+    for(auto el : ranges)
+    {
+        std::cout << el.value << ":" << el.count << "  ";
+        size += el.count;
+
+        if(el.value != -1)
+            occupiedSize += el.count;
+    }
+    std::cout << "size:" << size;
+    std::cout << "  occupied:" << occupiedSize;
+    std::cout << std::endl;
+}
+
 uint64_t Solve(const std::string& filename, int part)
 {
     uint64_t answer = 0;
     String input = GetFileAsSingleLine(filename);
 
-    std::vector<int> blocks;
+    std::vector<int> blocks;    // used for pt 1
+    std::list<Range> ranges;    // used for pt 2
 
     bool isFree = false;
     int blockNum = 0;
+
     for(auto c : input)
     {
         int val = c - '0';
+        Range range;
+
         if(isFree)
         {
             for(int i=0 ; i<val ; i++)
             {
                 blocks.push_back(-1);
             }
+            range.count = val;
+            range.value = -1;
+            ranges.push_back(range);
         }
         else
         {
@@ -35,6 +60,9 @@ uint64_t Solve(const std::string& filename, int part)
             {
                 blocks.push_back(blockNum);
             }
+            range.count = val;
+            range.value = blockNum;
+            ranges.push_back(range);
             blockNum++;
         }
         isFree = !isFree;
@@ -68,6 +96,57 @@ uint64_t Solve(const std::string& filename, int part)
     }
     else
     {
+        PrintRange(ranges);
+
+        std::list<Range>::iterator tail = ranges.end();
+        tail--;
+        std::list<Range>::iterator head = ranges.begin();
+        while(tail != ranges.begin())
+        {
+            if(tail->value != -1)
+            {
+                Range& tailRange = *tail;
+                // begin forward scan for a gap
+                head = ranges.begin();
+                bool found = false;
+                while((head != tail) && !found)
+                {
+                    if((head->value == -1) && (head->count >= tail->count))
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        head++;
+                    }
+                }
+                if(found)
+                {
+                    if(head->count == tail->count)
+                    {
+                        // simple move
+                        head->value = tail->value;
+                        tail->value = -1;
+                    }
+                    else
+                    {
+                        Range newRange; // new empty range
+                        newRange.value = -1;
+                        newRange.count = head->count - tail->count;
+                        std::list<Range>::iterator ins = head;
+                        ins++;
+                        ranges.insert(ins, newRange);
+                        head->count = tail->count;
+                        head->value = tail->value;
+                        tail->value = -1;
+                    }
+                }
+                tail--;
+                PrintRange(ranges);
+            }
+
+            tail--;
+        }
     }
 
 
@@ -76,9 +155,10 @@ uint64_t Solve(const std::string& filename, int part)
 
 int main(int argc, const char** argv)
 {
-    assert(Solve("../input/day9test", 1) == 1928);
-    assert(Solve("../input/day9test", 2) == 2858);
-    
+//    assert(Solve("../input/day9test", 1) == 1928);
+//    assert(Solve("../input/day9test", 2) == 2858);
+    Solve("../input/day9test", 2);
+
     assert(Solve("../input/day9", 1) == 6463499258318);
 //    assert(Solve("../input/day8", 2) == 1280);
 
