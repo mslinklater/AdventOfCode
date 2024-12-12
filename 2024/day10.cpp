617341894223ca60e4aa7b34489928db000770a9
Martin Linklater <mslinklater@gmail.com>
2024-12-12 09:27:18 +0000

    Day 10 done

--- 2024/day10.cpp
+++ 2024/day10.cpp
@@ -7,33 +7,72 @@
 
 struct Node
 {
+    Node(){}
+    virtual ~Node()
+    {
+        for(auto node : nextNodes)
+            delete node;
+    }
+
     Vec2 pos;
     int value;
-    std::vector<Node> nextNodes;
+    std::vector<Node*> nextNodes;
+
+    int GetRoutesFromHere()
+    {
+        if(value == 9)
+        {
+            return 1;
+        }
+        else
+        {
+            if(nextNodes.size() == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int sum = 0;
+                for(Node* next : nextNodes)
+                {
+                    sum += next->GetRoutesFromHere();
+                }
+                return sum;
+            }
+        }
+    }
+
+    static std::set<Vec2> nineSet;
 };
 
-void BuildNodeGraph(Node& node, const Grid2D<int>& map)
+std::set<Vec2> Node::nineSet;
+
+void BuildNodeGraph(Node* node, const Grid2D<int>& map)
 {
-    if(node.value == 9) return; // already found the top so abort the recursion
-
-    Vec2 searchMin(std::max(0, node.pos.x-1), std::max(0, node.pos.y-1));
-    Vec2 searchMax(std::min(map.Width(), node.pos.x+2), std::min(map.Height(), node.pos.y+2));
-
-    for(int x=searchMin.x; x < searchMax.x; ++x)
+    if(node->value == 9)
     {
-        for(int y=searchMin.y; y < searchMax.y; ++y)
+        Node::nineSet.insert(node->pos);
+        return;
+    }
+
+    std::vector<Vec2> searchPositions;
+
+    if(node->pos.x > 0) searchPositions.push_back(Vec2(node->pos.x-1, node->pos.y));
+    if(node->pos.x < map.Width() - 1) searchPositions.push_back(Vec2(node->pos.x+1, node->pos.y));
+    if(node->pos.y > 0) searchPositions.push_back(Vec2(node->pos.x, node->pos.y-1));
+    if(node->pos.y < map.Height() - 1) searchPositions.push_back(Vec2(node->pos.x, node->pos.y+1));
+
+    for(Vec2 pos : searchPositions)
+    {
+        int upValue = node->value + 1;
+        if(map.Get(pos.x, pos.y) == upValue)
         {
-            int upValue = node.value + 1;
-            if(map.Get(x, y) == upValue)
-            {
-                Node newNode;
-                newNode.pos.x = x;
-                newNode.pos.y = y;
-                newNode.value = upValue;
-                node.nextNodes.push_back(newNode);
-                Node& ref = *node.nextNodes.end();
-                BuildNodeGraph(ref, map);
-            }
+            Node* newNode = new Node();
+            newNode->pos.x = pos.x;
+            newNode->pos.y = pos.y;
+            newNode->value = upValue;
+            node->nextNodes.push_back(newNode);
+            BuildNodeGraph(newNode, map);
         }
     }
 }
@@ -46,7 +85,7 @@ uint64_t Solve(const std::string& filename, int part)
     int height = lines.size();
 
     Grid2D<int> map(width, height);
-    std::vector<Node> startNodes;
+    std::vector<Node*> startNodes;
 
     for(int y=0 ; y<height ; y++)
     {
@@ -55,17 +94,26 @@ uint64_t Solve(const std::string& filename, int part)
             map.Set(x, y, lines[y][x] - '0');
             if(map.Get(x, y) == 0)
             {
-                Node n;
-                n.pos = {x, y};
-                n.value = 0;
+                Node* n = new Node();
+                n->pos = {x, y};
+                n->value = 0;
                 startNodes.push_back(n);
             }
         }
     }
 
-    for(Node node : startNodes)
+    for(Node* node : startNodes)
     {
+        Node::nineSet.clear();
         BuildNodeGraph(node, map);
+        if(part == 1)
+        {
+            answer += Node::nineSet.size();
+        }
+        else
+        {
+            answer += node->GetRoutesFromHere();
+        }
     }
 
     return answer;
@@ -78,13 +126,13 @@ int main(int argc, const char** argv)
     assert(Solve("../input/day10test3", 1) == 4);
     assert(Solve("../input/day10test4", 1) == 3);
     assert(Solve("../input/day10test", 1) == 36);
-//    assert(Solve("../input/day9test", 2) == 2858);
+    assert(Solve("../input/day10test", 2) == 81);
 
-//    assert(Solve("../input/day9", 1) == 6463499258318);
-//    assert(Solve("../input/day9", 2) == 6493634986625);
+    assert(Solve("../input/day10", 1) == 617);
+    assert(Solve("../input/day10", 2) == 1477);
 
-    std::cout << "problem 1:" << Solve("../input/day9", 1) << std::endl;
-//    std::cout << "problem 2:" << Solve("../input/day9", 2) << std::endl;
+    std::cout << "problem 1:" << Solve("../input/day10", 1) << std::endl;
+    std::cout << "problem 2:" << Solve("../input/day10", 2) << std::endl;
 
     return 0;
 }
