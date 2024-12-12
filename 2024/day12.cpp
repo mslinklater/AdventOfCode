09585b1ac86c8913abec1f98b331ddb0403fbf6b
Martin Linklater <mslinklater@gmail.com>
2024-12-12 20:17:01 +0000

    day 12

--- 2024/day12.cpp
+++ 2024/day12.cpp
@@ -13,13 +13,13 @@ uint64_t Solve(const std::string& filename, int part)
 
 int main(int argc, const char** argv)
 {
-    assert(Solve("../input/day12test", 1) == 0);
+    assert(Solve("../input/day12test", 1) == 1930);
 //    assert(Solve("../input/day11test", 1, 25) == 55312);    // 25 times
 
 //    assert(Solve("../input/day11", 1, 25) == 216996);
 //    assert(Solve("../input/day11", 1, 75) == 257335372288947);
 
-    std::cout << "problem 1:" << Solve("../input/day11", 1) << std::endl;
+    std::cout << "problem 1:" << Solve("../input/day12", 1) << std::endl;
 //    std::cout << "problem 2:" << Solve("../input/day11", 1, 75) << std::endl;
 
     return 0;
