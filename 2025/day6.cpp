fc6cf9add6db21d5cb36bd1e684adda323c4daa5
Martin Linklater <mslinklater@gmail.com>
2025-12-13 21:16:01 +0000

    day6

--- 2025/day6.cpp
+++ 2025/day6.cpp
@@ -8,6 +8,22 @@ struct Calculation
 {
     std::vector<uint64_t> numbers;
     char operation;
+    uint64_t Answer() const
+    {
+        uint64_t result = numbers[0];
+        for(size_t i = 1; i < numbers.size(); i++)
+        {
+            if(operation == '+')
+            {
+                result += numbers[i];
+            }
+            else if(operation == '*')
+            {
+                result *= numbers[i];
+            }
+        }
+        return result;
+    }
 };
 
 std::vector<Calculation> calculations;
@@ -23,37 +39,62 @@ void ParseFile(const std::string& filename, int calcSize)
         {
             // read numbers
             StringVector parts = SplitStringByChar(line, ' ');
+            int calculationIndex = 0;
             for(const String& p : parts)
             {
+                if(p.empty()) continue;
+
                 if(lineNumber == 0)
                 {
                     calculations.push_back(Calculation());
                 }
-                calculations[lineNumber].numbers.push_back(std::stoll(p));
+                calculations[calculationIndex].numbers.push_back(std::stoll(p));
+                calculationIndex++;
             }
         }
         else
         {
+            int calculationIndex = 0;
+            for(char c : line)
+            {
+                if(c == '+' || c == '*')
+                {
+                    calculations[calculationIndex].operation = c;
+                    calculationIndex++;
+                }
+            }
             // read operation
         }
+        lineNumber++;
     }
 }
 
-uint64_t Solve(const std::string& filename, int part)
+uint64_t Solve(const std::string& filename, int part, bool isTest)
 {
     uint64_t answer = 0;
+    ParseFile(filename, isTest ? 3 : 4);
+    if(part ==1)
+    {
+        for(const Calculation& calc : calculations)
+        {
+            answer += calc.Answer();
+        }
+    }
+    else
+    {
+    }
     return answer;
 }
 
 int main(int argc, const char** argv)
 {
-    assert(Solve("../input/day6test", 1) == 4277556);
-    //assert(Solve("../input/day4", 1) == 1537);
+    assert(Solve("../input/day6test", 1, true) == 4277556);
+    assert(Solve("../input/day6", 1, false) == 5316572080628);
 
-//    assert(Solve("../input/day6test", 2) == 14);
+    assert(Solve("../input/day6test", 2, true) == 3263827);
 //    assert(Solve("../input/day4", 2) == 8707);
 
-    std::cout << "problem 1:" << Solve("../input/day6", 1) << std::endl;
+    std::cout << "problem 1:" << Solve("../input/day6", 1, false) << std::endl;
 //    std::cout << "problem 2:" << Solve("../input/day5", 2) << std::endl;
 
     return 0;
